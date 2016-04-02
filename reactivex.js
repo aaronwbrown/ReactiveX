daf94633f15409bf49c4d64bc753e819bae7dbef
aaronwbrown <aaron.w.brown13@gmail.com>
2016-04-02 16:51:05 -0700

    Completed Exc.11 - Use map() and concatAll() to to project and flatten movieList IDs.

--- reactivex.js
+++ reactivex.js
@@ -420,8 +420,75 @@ Array.prototype.concatAll = function() {
 
 };
 
-// JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()) === "[1,2,3,4,5,6,7,8,9]"
-// [1,2,3].concatAll(); // throws an error because this is a one-dimensional array
+/*
+
+concatAll is a very simple function, so much so that it may not be obvious yet how it can be combined with map() to query a tree. Let's try an example...
+
+Exercise 11: Use map() and concatAll() to project and flatten the movieLists into an array of video ids
+
+Hint: use two nested calls to map() and one call to concatAll().
+
+*/
+
+function() {
+  var movieLists = [
+      {
+        name: "New Releases",
+        videos: [
+          {
+            "id": 70111470,
+            "title": "Die Hard",
+            "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+            "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 4.0,
+            "bookmark": []
+          },
+          {
+            "id": 654356453,
+            "title": "Bad Boys",
+            "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+            "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 5.0,
+            "bookmark": [{ id:432534, time:65876586 }]
+          }
+        ]
+      },
+      {
+        name: "Dramas",
+        videos: [
+          {
+            "id": 65432445,
+            "title": "The Chamber",
+            "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+            "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 4.0,
+            "bookmark": []
+          },
+          {
+            "id": 675465,
+            "title": "Fracture",
+            "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+            "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+            "rating": 5.0,
+            "bookmark": [{ id:432534, time:65876586 }]
+          }
+        ]
+      }
+    ];
+
+
+
+  // ------------   INSERT CODE HERE!  -----------------------------------
+  // Use map and concatAll to flatten the movieLists in a list of video ids.
+  // ------------   INSERT CODE HERE!  -----------------------------------
+
+  return movieLists.map(function(movieTree) {
+    return movieTree.videos.map(function(movieId) {
+      return movieId.id;
+    });
+  }).concatAll();
+}
+
 
 
 
